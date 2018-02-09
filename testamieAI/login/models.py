115c9e85346c8a269c80bed369ae417790e3827a
bito <bito@pol-japan.co.jp>
2018-02-09 20:21:01 +0900

    vue.js umakuikanakattayatu

---
+++ testamieAI/login/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Stock(models.Model):
+
+    class Meta:
+        db_table = "stock"
+    id = models.AutoField(primary_key=True)
+    title = models.TextField()
+    stock_count = models.IntegerField()
---
+++ testamieAI/login/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+import os
+
+from django.conf import settings
+from django.http.response import HttpResponse
+from rest_framework import viewsets
+
+from login.models import Stock
+from login.serializer import StockSerializer
+
+# 静的ファイルを返すView
+def index(_):
+    # render等でDjangoのtemplateとして処理すると「{{}}」がVue.jsに渡る前消えてしまう。
+    # 良い解決方法が浮かばなかったので、static配下に置いたファイルをopenして投げることで回避。
+    html = open(
+        os.path.join(settings.STATICFILES_DIRS[0], "vue_grid.html")).read()
+    return HttpResponse(html)
+
+# RestAPIのviewsets
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # APIのフィルタで使えるフィールドを指定
+    filter_fields = ("id", "title", 'stock_count')
